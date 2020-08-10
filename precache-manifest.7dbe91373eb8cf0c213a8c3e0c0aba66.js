8fd83d75888e11cc05be42b8d9495f18b1de7780
subhadipghs <ghoshsubhadipghosh@gmail.com>
2020-08-10 21:34:55 +0530

    Updates

---
+++ precache-manifest.7dbe91373eb8cf0c213a8c3e0c0aba66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0e1a8c81af696dfd925a02e2d04cbf",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "915d89f4685bb4a9501e",
+    "url": "/recipe-app/static/css/main.3257cd34.chunk.css"
+  },
+  {
+    "revision": "570d5b0877bbe5870e53",
+    "url": "/recipe-app/static/js/2.d301cba0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.d301cba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915d89f4685bb4a9501e",
+    "url": "/recipe-app/static/js/main.dcb704ec.chunk.js"
+  },
+  {
+    "revision": "fb95e24ec9a88392efe2",
+    "url": "/recipe-app/static/js/runtime-main.f0725f22.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/recipe-app/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/recipe-app/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "fd3a054f1abfa8be8cc81359718f3ca6",
+    "url": "/recipe-app/static/media/Illustration3.fd3a054f.png"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/recipe-app/static/media/Illustration4.df92c811.png"
+  },
+  {
+    "revision": "32b67c728ad8b691637e6832b8b11cef",
+    "url": "/recipe-app/static/media/Img2.32b67c72.jpg"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "/recipe-app/static/media/Metropolis-Black.de55ae52.otf"
+  },
+  {
+    "revision": "f7b5e589f88206b4bd5cb1408c5362e6",
+    "url": "/recipe-app/static/media/Metropolis-Regular.f7b5e589.otf"
+  }
+]);
